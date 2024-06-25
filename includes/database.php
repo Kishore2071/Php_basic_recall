d9b17c2937dfc1115b110bfd1838b3926060ba9d
Kishore2071 <kishorekumarsaran2001@gmail.com>
2024-06-25 16:08:25 +0530

    implemented CRUD in articles

--- includes/database.php
+++ includes/database.php
@@ -1,13 +1,23 @@
 <?php
 
-$db_host = "localhost";
-$db_name = "cms";
-$db_user = "test";
-$db_pass = "Password@12345";
+/**
+ * Get the database connection
+ *
+ * @return object Connection to the database server
+ */
+function getDB()
+{
+    $db_host = "localhost";
+    $db_name = "cms";
+    $db_user = "test";
+    $db_pass = "Password@12345";
 
-$conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
+    $conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
 
-if (mysqli_connect_error()) {
-    echo mysqli_connect_error();
-    exit;
+    if (mysqli_connect_error()) {
+        echo mysqli_connect_error();
+        exit;
+    }
+
+    return $conn;
 }
