c30d63725859a2de375ef3bb9ffa1a01e96c0d68
Kishore2071 <kishorekumarsaran2001@gmail.com>
2024-06-29 16:05:34 +0530

    Replace CRUD DB with PDO

--- classes/Database.php
+++ classes/Database.php
@@ -22,8 +22,8 @@ class Database
         $dsn = 'mysql:host=' . $db_host . ';dbname=' . $db_name . ';charset=utf8';
 
         try {
-            $db = new PDO($dsn, $db_user, $db_pass);
 
+            $db = new PDO($dsn, $db_user, $db_pass);
             $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
             return $db;
