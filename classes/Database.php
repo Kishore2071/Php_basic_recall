675935b776788ea210cb8223a19dda24afc03d7f
Kishore2071 <kishorekumarsaran2001@gmail.com>
2024-06-30 14:06:33 +0530

    Pagination

--- classes/Database.php
+++ classes/Database.php
@@ -16,8 +16,8 @@ class Database
     {
         $db_host = "localhost";
         $db_name = "cms";
-        $db_user = "cms_www";
-        $db_pass = "64w6H2rOJ1zwLRyk";
+        $db_user = "test";
+        $db_pass = "Password@12345";
 
         $dsn = 'mysql:host=' . $db_host . ';dbname=' . $db_name . ';charset=utf8';
 
